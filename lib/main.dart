1711e6810bfe01d723e98e7acc42692d96cfb2db
due.nguyencanhduc <due.nguyencanhduc@mbs.com.vn>
2024-12-31 08:13:36 +0700

    update firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 var logger = Logger(
   printer: PrettyPrinter(),
 );
 
-
 void main() {
-  logger.d("Debug log");
   runApp(MyApp());
 }
 
@@ -24,7 +23,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class MyWebViewPage extends StatefulWidget {
   const MyWebViewPage({super.key});
 
@@ -34,55 +32,102 @@ class MyWebViewPage extends StatefulWidget {
 
 class _MyWebViewPageState extends State<MyWebViewPage> {
   late InAppWebViewController _webViewController;
-  late InAppWebView _webView;
+  String currentLink = "https://truyenfull.io";
   final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    readMyNoteInSharedPreferences();
+  }
+
+  Future<void> readMyNoteInSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? savedLink = prefs.getString("current_link");
+    setState(() {
+      currentLink = savedLink ?? "https://truyenfull.io";
+    });
+    logger.d("Load currentLink: $savedLink");
+    _webViewController.loadUrl(
+        urlRequest: URLRequest(url: WebUri(currentLink)));
+  }
+
+  Future<void> _saveCurrentLink(String text) async {
+    logger.d("Save page $text");
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("current_link", text);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                  hintText: 'Enter URL...',
-                  border: InputBorder.none,
-                  hintStyle: TextStyle(color: Colors.greenAccent),
-                ),
-                style: TextStyle(color: Colors.blueGrey),
-                onSubmitted: (value) {
-                  // Khi người dùng nhấn Enter, tải lại trang với URL mới
-                  if (Uri.tryParse(value)?.hasAbsolutePath == true) {
-                    logger.d("Load URL $value");
-                    _webViewController.loadUrl(urlRequest: URLRequest(url: WebUri(value)));
-                  } else {
-                    logger.d("URL $value invalid" );
-                  }
-                },
+        appBar: AppBar(
+          title: Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Enter URL...',
+                border: InputBorder.none,
+                hintStyle: TextStyle(color: Colors.greenAccent),
               ),
+              style: TextStyle(color: Colors.blueGrey),
+              onSubmitted: (value) {
+                // Khi người dùng nhấn Enter, tải lại trang với URL mới
+                if (Uri.tryParse(value)?.hasAbsolutePath == true) {
+                  logger.d("Load URL $value");
+                  _webViewController.loadUrl(
+                      urlRequest: URLRequest(url: WebUri(value)));
+                  _saveCurrentLink(value);
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Saved page!")),
+                  );
+                } else {
+                  logger.d("URL $value invalid");
+                }
+              },
+            ),
+          ),
+        ),
+        body: Stack(
+          children: [
+            InAppWebView(
+                initialUrlRequest: URLRequest(url: WebUri(currentLink)),
+                initialSettings: InAppWebViewSettings(
+                  javaScriptEnabled: false,
+                  clearCache: true,
+                  userAgent: "CustomUserAgent",
+                  useHybridComposition: true, // Áp dụng cho Android
+                  useShouldOverrideUrlLoading:
+                  true, // Kích hoạt chặn điều hướng
+                  javaScriptCanOpenWindowsAutomatically:
+                  false, // Chặn JavaScript mở popup
+                ),
+                onWebViewCreated: (controller) {
+                  logger.d("onWebViewCreated");
+                  _webViewController = controller;
+                },
+                onLoadStop: (controller, url) async {
+                  logger.d("onLoadStop: $url");
+                }),
+            Positioned(
+                right: 16.0,
+                bottom: 32.0,
+                child: SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: FloatingActionButton(
+                      child: const Icon(Icons.bookmarks),
+                      onPressed: () async {
+                        Uri? url = await _webViewController.getOriginalUrl();
+                        String currentUrl = url.toString();
+                        logger.d("onPressed FAB url=$currentUrl");
+                        // _saveCurrentLink(url?.toString() ?? "");
+                      }),
+                )
             ),
           ],
         ),
-      ),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri("https://truyenfull.io")),
-        initialSettings: InAppWebViewSettings(
-          javaScriptEnabled: false,
-          clearCache: true,
-          userAgent: "CustomUserAgent",
-          useHybridComposition: true, // Áp dụng cho Android
-          useShouldOverrideUrlLoading: true, // Kích hoạt chặn điều hướng
-          javaScriptCanOpenWindowsAutomatically: false, // Chặn JavaScript mở popup
-        ),
-        onWebViewCreated: (controller) {
-          _webViewController = controller;
-        },
-        onLoadStop: (controller, url) async {
-          logger.d("Load done");
-        },
-      ),
-    );
+        );
   }
 }
